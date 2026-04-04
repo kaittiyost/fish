28a9ecfd26b7aadbb8f2562e30ce8812e3df5462
alan <kiattiyost.oke@gmail.com>
2026-04-04 22:09:26 +0700

    imp php8 r2

--- project_it/connect/connect.php
+++ project_it/connect/connect.php
@@ -1,100 +1,19 @@
 <?php
-	$host="localhost";
-	$dbuser="root";
-	$password="abcd1234";
-	$dbname="project_it";
-	
-	// Create global mysqli connection for compatibility shim
-	$global_mysqli_conn = @mysqli_connect($host, $dbuser, $password) or die ("Not Connect Database");
-	@mysqli_select_db($global_mysqli_conn, $dbname);
-	@mysqli_query($global_mysqli_conn, "SET NAMES 'utf8'");
+$host = "127.0.0.1";
+$dbuser = "fishweb";
+$password = "12345";
+$dbname = "fishdb";
 
-	// --- MySQL Compatibility Shim for PHP 7/8 ---
+// สร้างการเชื่อมต่อ
+$conn = new mysqli($host, $dbuser, $password, $dbname);
 
-	if (!function_exists('mysql_connect')) {
-		function mysql_connect($host, $dbuser, $password) {
-			global $global_mysqli_conn;
-			return $global_mysqli_conn;
-		}
-	}
+// ตรวจสอบการเชื่อมต่อ
+if ($conn->connect_error) {
+	die("Not Connect Database: " . $conn->connect_error);
+}
 
-	if (!function_exists('mysql_select_db')) {
-		function mysql_select_db($dbname, $conn = null) {
-			global $global_mysqli_conn;
-			return mysqli_select_db($global_mysqli_conn, $dbname);
-		}
-	}
+// ตั้งค่า charset
+$conn->set_charset("utf8");
 
-	if (!function_exists('mysql_query')) {
-		function mysql_query($sql, $conn = null) {
-			global $global_mysqli_conn;
-			return mysqli_query($global_mysqli_conn, $sql);
-		}
-	}
-
-	if (!function_exists('mysql_fetch_array')) {
-		function mysql_fetch_array($result, $result_type = MYSQLI_BOTH) {
-            if (!is_object($result)) return false;
-			return mysqli_fetch_array($result, $result_type);
-		}
-	}
-
-	if (!function_exists('mysql_fetch_assoc')) {
-		function mysql_fetch_assoc($result) {
-            if (!is_object($result)) return false;
-			return mysqli_fetch_assoc($result);
-		}
-	}
-
-	if (!function_exists('mysql_fetch_object')) {
-		function mysql_fetch_object($result) {
-            if (!is_object($result)) return false;
-			return mysqli_fetch_object($result);
-		}
-	}
-
-	if (!function_exists('mysql_fetch_row')) {
-		function mysql_fetch_row($result) {
-            if (!is_object($result)) return false;
-			return mysqli_fetch_row($result);
-		}
-	}
-
-	if (!function_exists('mysql_num_rows')) {
-		function mysql_num_rows($result) {
-            if (!is_object($result)) return false;
-			return mysqli_num_rows($result);
-		}
-	}
-
-	if (!function_exists('mysql_insert_id')) {
-		function mysql_insert_id($conn = null) {
-			global $global_mysqli_conn;
-			return mysqli_insert_id($global_mysqli_conn);
-		}
-	}
-
-	if (!function_exists('mysql_error')) {
-		function mysql_error($conn = null) {
-			global $global_mysqli_conn;
-			return mysqli_error($global_mysqli_conn);
-		}
-	}
-
-	if (!function_exists('mysql_close')) {
-		function mysql_close($conn = null) {
-			global $global_mysqli_conn;
-			if ($global_mysqli_conn) {
-				return mysqli_close($global_mysqli_conn);
-			}
-			return true;
-		}
-	}
-
-	if (!function_exists('mysql_real_escape_string')) {
-		function mysql_real_escape_string($string, $conn = null) {
-			global $global_mysqli_conn;
-			return mysqli_real_escape_string($global_mysqli_conn, $string);
-		}
-	}
+echo "Connect Success";
 ?>
